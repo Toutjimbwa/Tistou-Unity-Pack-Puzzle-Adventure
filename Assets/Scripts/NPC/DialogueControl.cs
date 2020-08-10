336a91bdd074cc62a02685f943342d946d1749bc
Tistou Blomberg <tistou.blomberg@gmail.com>
2020-08-10 19:31:13 +0200

    Started thinking about dialogueing

---
+++ Assets/Scripts/NPC/DialogueControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TistouUnity
+{
+    namespace PuzzleAdventurePack
+    {
+        public class DialogueControl : MonoBehaviour
+        {
+            public void OpenDialogue()
+            {
+                //Setup for a dialogue, open UI etc.
+                //Multiple choices should be available.
+            }
+            public void Say(string name, string text)
+            {
+                Debug.Log($"{name}: {text}");
+            }
+        }
+    }
+}
