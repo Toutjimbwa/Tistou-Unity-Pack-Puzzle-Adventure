b54698091fde7ad83d3621adb7ed31a1311c721a
Tistou Blomberg <tistou.blomberg@gmail.com>
2020-08-10 18:26:07 +0200

    Started with Adventures

---
+++ Assets/Scripts/Adventure/Adventure.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TistouUnity
+{
+    namespace PuzzleAdventurePack
+    {
+        public class Adventure : MonoBehaviour
+        {
+            public enum State
+            {
+                Hidden,
+                Available,
+                Started,
+                Done
+            }
+
+        }
+    }
+}
