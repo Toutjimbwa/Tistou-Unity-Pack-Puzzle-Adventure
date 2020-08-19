8a397935ea5802f4eecfde2018b81586d76b88b0
Tistou Blomberg <tistou.blomberg@gmail.com>
2020-08-19 16:46:08 +0200

    Cleaned up Warp, made a new scene inside mountain. found bug: camera doesnt reset in new scene.

--- Assets/Scripts/Warp/Warp.cs
+++ Assets/Scripts/Warp/Warp.cs
@@ -7,10 +7,10 @@ namespace TistouUnity
     {
         public class Warp : MonoBehaviour
         {
-            [SerializeField] private GameObject _playerPrefab = null;
             [SceneName] public string EnterScene;
             [SerializeField] private WarpTag _warpTag = WarpTag.A;
             private Transform _enterPoint;
+            private GameObject _player;
             private void OnEnable()
             {
                 transform.parent = null;
@@ -28,8 +28,9 @@ namespace TistouUnity
                 if (other.CompareTag("Player"))
                 {
                     GetComponent<Collider>().enabled = false;
-                    DontDestroyOnLoad(gameObject);
-                    SceneManager.sceneLoaded += InstantiatePlayer;
+                    _player = GameObject.FindGameObjectWithTag("Player");
+                    _player.SetActive(false);
+                    SceneManager.sceneLoaded += PlacePlayer;
                     SceneManager.LoadScene(EnterScene);
                 }
             }
@@ -48,20 +49,13 @@ namespace TistouUnity
                 }
                 return null;
             }
-            private void InstantiatePlayer(Scene scene, LoadSceneMode mode)
+            private void PlacePlayer(Scene scene, LoadSceneMode mode)
             {
-                SceneManager.sceneLoaded -= InstantiatePlayer;
-                var player = GameObject.FindGameObjectWithTag("Player");
-                if (!player)
-                {
-                    player = Instantiate(_playerPrefab);
-                }
-                player.GetComponent<Disableable>().DisableComponentsButNotTransform();
+                SceneManager.sceneLoaded -= PlacePlayer;
                 var exit = FindWarpExit();
-                player.transform.position = exit._enterPoint.transform.position;
-                player.transform.rotation = exit._enterPoint.transform.rotation;
-                player.GetComponent<Disableable>().EnableComponentsButNotTransform();
-                Destroy(gameObject);
+                _player.transform.position = exit._enterPoint.transform.position;
+                _player.transform.rotation = exit._enterPoint.transform.rotation;
+                _player.SetActive(true);
             }
         }
     }
